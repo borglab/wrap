fdd7b8cade7c2dfb01e25da7da878f95c2ab3146
Varun Agrawal <varagrawal@gmail.com>
2021-04-19 15:00:42 -0400

    fixes

--- gtwrap/interface_parser/variable.py
+++ gtwrap/interface_parser/variable.py
@@ -7,7 +7,7 @@ See LICENSE for the license information
 
 Parser classes and rules for parsing C++ variables.
 
-Author: Duy Nguyen Ta, Fan Jiang, Matthew Sklar, Varun Agrawal, and Frank Dellaert
+Author: Varun Agrawal, Gerry Chen
 """
 
 from .tokens import IDENT, SEMI_COLON
@@ -15,13 +15,16 @@ from .type import TemplatedType, Type
 
 class Variable:
     """
-    Rule to parse the variable members of a class.
+    Rule to parse variables.
+    Variables are a combination of Type/TemplatedType and the variable identifier.
 
     E.g.
     ```
     class Hello {
-        string name;  // This is a property.
+        string name;  // This is a property variable.
     };
+
+    Vector3 kGravity;  // This is a global variable.
     ````
     """
     rule = (
